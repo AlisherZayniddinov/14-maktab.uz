a17340ccd0c1f6a5f0f6eb43a573d1556f1be6c4
Alisher <zayniddinov.alisher@gmail.com>
2025-04-20 23:05:58 +0500

    Initial commit with website files

---
+++ script.js
@@ -0,0 +1,7 @@
+// Sayt ishga tushganidan so'ng konsolga habar chiqarish
+console.log("Sayt ishga tushdi!");
+
+// JavaScript orqali interaktivlik qo'shish
+document.querySelector("h1").addEventListener("click", function() {
+    alert("Siz 14-Maktab logotipini bosdingiz!");
+});
